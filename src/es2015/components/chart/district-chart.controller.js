6bb36f6d35286a9bf26cd2ca6bc26749af63ddfb
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2016-04-24 08:26:52 +0200

    Zmiana kolorów wykresu + wycentrowanie etykiet osi poziomej (implementacja woła o pomstę do nieba, to trzeba będzie potem zrobić porządnie) + zmiana formatu liczb osi pionowej (usunięcie części ułamkowej)

--- src/es2015/components/chart/district-chart.controller.js
+++ src/es2015/components/chart/district-chart.controller.js
@@ -3,13 +3,18 @@ function districtChartCtrl(districtChartDataService, DTOptionsBuilder, DTColumnB
     this.districtEvents = {
         draw: (data) => {
             if (data.type === 'label' && data.text.indexOf(' %') === -1) {
+                // this is weird, but I have no idea how to implement it in a smart way
+                var offsetToMakeTextCentered = 0;
+                if (data.text.indexOf(' ') === -1) {
+                    offsetToMakeTextCentered = -12;
+                }
                 // adjust label position for rotation
-                const dX = data.width * 0.9 + (100 - data.width);
+                const dX = data.width * 0.9 + (100 - data.width) + offsetToMakeTextCentered;
                 data.element.attr({ x: data.element.attr('x') - dX })
             }
             if(data.type === 'bar') {
                 data.element.attr({
-                    style: 'stroke-width: 40px; stroke: #0000ff'
+                    style: 'stroke-width: 40px; stroke: #339933'
                 });
             }
         }
@@ -25,7 +30,7 @@ function districtChartCtrl(districtChartDataService, DTOptionsBuilder, DTColumnB
                     low: 0,
                     axisY: {
                         offset: 80,
-                        labelInterpolationFnc: (value) => value.toFixed(1) + ' %',
+                        labelInterpolationFnc: (value) => value.toFixed(0) + ' %',
                         scaleMinSpace: 50
                     }
                 };
